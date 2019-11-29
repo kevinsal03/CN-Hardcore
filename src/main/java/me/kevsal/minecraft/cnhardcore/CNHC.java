0ada05449617133fbee0f93ce7761619ca8d35e9
Kevinsal03 <kevinsal03@users.noreply.github.com>
2019-11-29 12:48:27 -0500

    plugin shell

--- src/main/java/me/kevsal/minecraft/cnhardcore/CNHC.java
+++ src/main/java/me/kevsal/minecraft/cnhardcore/CNHC.java
@@ -3,4 +3,14 @@ package me.kevsal.minecraft.cnhardcore;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CNHC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
